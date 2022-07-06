b482dbe741d3e8b4e85b2a12a2509fdcfa10c8cb
juancamilomejia <juamej@eltiempo.com>
2022-07-06 02:14:28 -0500

    add: the model for the news is added

---
+++ NewsWidget/Model/NewsModel.swift
@@ -0,0 +1,38 @@
+//
+//  NewsModel.swift
+//  NewsWidget
+//
+//  Created by MacBook J&J  on 6/07/22.
+//
+
+import Foundation
+
+// MARK: - News
+struct News: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let author: String?
+    let title, articleDescription: String
+    let url: String
+    let urlToImage: String?
+    let publishedAt: Date
+    let content: String
+
+    enum CodingKeys: String, CodingKey {
+        case source, author, title
+        case articleDescription = "description"
+        case url, urlToImage, publishedAt, content
+    }
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String
+}
