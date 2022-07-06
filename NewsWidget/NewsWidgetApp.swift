8add8cd583f760f3b8d9d6577ed395357198b2ea
juancamilomejia <juamej@eltiempo.com>
2022-07-06 02:00:16 -0500

    Initial Commit

---
+++ NewsWidget/NewsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWidgetApp.swift
+//  NewsWidget
+//
+//  Created by MacBook J&J  on 6/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
